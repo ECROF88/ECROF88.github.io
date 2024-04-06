f8d0ff6f06844b14ee59126542913def6b6d13a0
ECROF88 <maomao8672@outlook.com>
2024-04-06 22:34:22 +0800

    Update zh.ts

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -13,8 +13,8 @@ export const zhNavbar = navbar([
         icon: "pen-to-square",
         prefix: "apple/",
         children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
+          { text: "1", icon: "pen-to-square", link: "1" },
+          { text: "2", icon: "pen-to-square", link: "2" },
           "3",
           "4",
         ],
