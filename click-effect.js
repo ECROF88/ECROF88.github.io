307e63b82fe6b739ce0565f201ecdf10ad3689c8
ECROF88 <114860152+ECROF88@users.noreply.github.com>
2024-04-07 13:16:19 +0000

    Deploying to gh-pages from @ ECROF88/ECROF88.github.io@442e9a5cb7a1c732a289ae2a0af4f505a8188c1d 🚀

---
+++ click-effect.js
@@ -0,0 +1,12 @@
+/* click-effect.js */
+document.addEventListener('click', function (event) {
+  var sparkle = document.createElement('div');
+  sparkle.className = 'firework';
+  sparkle.style.top = event.clientY-5 + 'px';
+  sparkle.style.left = event.clientX-6 + 'px';
+  document.body.appendChild(sparkle);
+  console.log(event.clientX, event.clientY);
+  setTimeout(function () {
+    document.body.removeChild(sparkle);
+  }, 1000);
+});
