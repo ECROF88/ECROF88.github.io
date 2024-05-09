774d2da1b00c3571b8f8e1b4d325f00c7a997eac
ECROF88 <114860152+ECROF88@users.noreply.github.com>
2024-05-09 11:16:24 +0000

    Deploying to gh-pages from @ ECROF88/ECROF88.github.io@ded4aa3da6502293ccc04568c55b7990ab479cb6 🚀

--- click-effect.js
+++ click-effect.js
@@ -10,3 +10,39 @@ document.addEventListener('click', function (event) {
     document.body.removeChild(sparkle);
   }, 1000);
 });
+
+//鼠标点击出现社会主义核心价值观文字特效
+/*使用了jQuery语法，若页面未引入JQ应引入JQ或者
+将本代码中JQ选择器替换为JS原生选择器*/
+var a_idx = 0;
+    $("body").click(function(e) {
+        var a = new Array(
+            "富强", "民主", "文明", "和谐",
+            "自由", "平等", "公正", "法治",
+            "爱国", "敬业", "诚信", "友善"
+            );
+        const col = [
+        "#FF0000","#FF7F00","#FFFF00","#00FF00","#00FFFF","#0000FF","#8B00FF"
+        ];
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 144469,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": col[Math.floor((Math.random()*col.length))]
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+        1500,
+        function() {
+            $i.remove()
+        })
+    });
