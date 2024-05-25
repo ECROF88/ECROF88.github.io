d60450666c836472d5099cf2ea8fa8ae0ef10d62
ECROF88 <114860152+ECROF88@users.noreply.github.com>
2024-05-25 07:25:56 +0000

    Deploying to gh-pages from @ ECROF88/ECROF88.github.io@ccbd4dbb5a235cd622d4b757c4ccd79eb6fcac63 🚀

--- click-effect.js
+++ click-effect.js
@@ -55,7 +55,31 @@
     window.onclick = function(event){
       old && old();
       // createHeart(event);
-      createTexts(event);
+      // createTexts(event);
+      // for(let i=0;i<20;i++)
+      // {
+      //   let spark = document.createElement('i');
+      //   spark.classList.add('spark');
+      //   const randomX=(Math.random()-0.5)*200;
+      //   const randomY=(Math.random()-0.5)*200;
+      //   spark.style.setProperty('--x',randomX+'px');
+      //   spark.style.setProperty('--y',randomY+'px');
+      //   const randomSize =Math.random()*8+2;
+      //   spark.style.width=randomSize+ 'px';
+      //   spark.style.height=randomSize+ 'px';
+      //   const duration= Math.random()*2 +0.5;
+      //   spark.style.animation=`animate ${duration}s
+      //   ease-out forward`;
+      //   document.body.appendChild(spark);
+      //   setTimeout(function(){
+      //     spark.remove();
+      //   },2000);
+      // }
+
+
+
+
+
     }
     // ==========================================================================================================
     // 把原本的点击出现爱心改为移动出现爱心/颜文字
